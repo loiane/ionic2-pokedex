38abee2c4773e326b820d4d40a76c95837b7e0d2
loiane <loianeg@gmail.com>
2016-09-07 17:47:38 -0300

    added pokemon detail sections

--- app/pages/pokemon-detail/pokemon-detail.ts
+++ app/pages/pokemon-detail/pokemon-detail.ts
@@ -5,10 +5,21 @@ import { PokedexService } from '../../providers/pokedex-service/pokedex-service'
 import { Capitalize } from '../../pipes/capitalize';
 import { PokeNumber } from '../../pipes/pokeNumber';
 
+import { PokemonDetailSprites } from '../../components/pokemon-detail-sprites/pokemon-detail-sprites';
+import { PokemonDetailAbilities } from '../../components/pokemon-detail-abilities/pokemon-detail-abilities';
+import { PokemonDetailStats } from '../../components/pokemon-detail-stats/pokemon-detail-stats';
+import { PokemonDetailMoves } from '../../components/pokemon-detail-moves/pokemon-detail-moves';
+
 @Component({
   templateUrl: 'build/pages/pokemon-detail/pokemon-detail.html',
   providers: [ PokedexService ],
-  pipes: [ Capitalize, PokeNumber ]
+  pipes: [ Capitalize, PokeNumber ],
+  directives: [
+    PokemonDetailSprites,
+    PokemonDetailAbilities,
+    PokemonDetailStats,
+    PokemonDetailMoves
+  ]
 })
 export class PokemonDetailPage {
 
