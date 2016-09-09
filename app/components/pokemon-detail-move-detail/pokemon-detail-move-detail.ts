e45bd08c7cdacd4fb815bf8bfddc88d7c212f9a5
loiane <loianeg@gmail.com>
2016-09-09 19:23:05 -0300

    enhanced pokemon move details

--- app/components/pokemon-detail-move-detail/pokemon-detail-move-detail.ts
+++ app/components/pokemon-detail-move-detail/pokemon-detail-move-detail.ts
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
 export class PokemonDetailMoveDetail {
 
   @Input() list: any[];
+  @Input() isEgg: boolean = false;
 
   ngOnInit(){
     //console.log(this.list);
