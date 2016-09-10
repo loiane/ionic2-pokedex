f9eeb07ff5c2a1a00083a0d586f42c5fbda59cfa
loiane <loianeg@gmail.com>
2016-09-10 11:45:46 -0300

    added type effectiveness details

---
+++ app/components/pokemon-detail-type-effectiveness/pokemon-detail-type-effectiveness.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+/*
+  Generated class for the PokemonDetailTypeEffectiveness component.
+
+  See https://angular.io/docs/ts/latest/api/core/ComponentMetadata-class.html
+  for more info on Angular 2 Components.
+*/
+@Component({
+  selector: 'pokemon-detail-type-effectiveness',
+  templateUrl: 'build/components/pokemon-detail-type-effectiveness/pokemon-detail-type-effectiveness.html'
+})
+export class PokemonDetailTypeEffectiveness {
+
+  text: string;
+
+  constructor() {
+    this.text = 'Hello World';
+  }
+}
