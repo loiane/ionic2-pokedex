1651ece2cc6112f3d0da6b4613930d557ad9f141
loiane <loianeg@gmail.com>
2016-09-10 09:30:59 -0300

    added moves service

--- app/pages/pokemon-list/pokemon-list.ts
+++ app/pages/pokemon-list/pokemon-list.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { PokedexService } from '../../providers/pokedex-service/pokedex-service';
+import { MovesService } from '../../providers/moves-service/moves-service';
+
 import { Loader } from '../../components/loader/loader';
 import { PokemonListItem } from '../pokemon-list-item/pokemon-list-item';
 import { PokemonDetailPage } from '../pokemon-detail/pokemon-detail';
 
 @Component({
   templateUrl: 'build/pages/pokemon-list/pokemon-list.html',
-  providers: [ PokedexService ],
+  providers: [ PokedexService, MovesService ],
   directives: [ Loader, PokemonListItem ]
 })
 export class PokemonListPage implements OnInit {
@@ -16,14 +18,20 @@ export class PokemonListPage implements OnInit {
   private pokemonList: any[] = [];
   private searchQuery: string = '';
 
+  private moves: any[] = [];
+
   constructor(
     private navCtrl: NavController,
-    private pokedexService: PokedexService
+    private pokedexService: PokedexService,
+    private movesService: MovesService
   ) {}
 
   ngOnInit(){
     this.pokedexService.getAllPokemon()
       .subscribe(pokemonList => this.pokemonList = pokemonList);
+
+    this.movesService.getMoves()
+      .subscribe(moves => this.moves = moves);
   }
 
   getPokemons(){
@@ -41,7 +49,8 @@ export class PokemonListPage implements OnInit {
 
   goToPokemonDetail(event){
     this.navCtrl.push(PokemonDetailPage, {
-      pokemon: event.pokemon
+      pokemon: event.pokemon,
+      moves: this.moves
     });
   }
 
