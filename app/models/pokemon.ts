2fee16fccbc347d7ba873b7b350e46e1a49fde42
loiane <loianeg@gmail.com>
2016-09-12 19:08:48 -0300

    added Pokemon model according to pokeapi

---
+++ app/models/pokemon.ts
@@ -0,0 +1,16 @@
+export class Pokemon {
+    id: Number
+    name: String
+    base_experience: Number
+    height: Number
+    is_default: Boolean
+    order: Number
+    weight: Number
+
+    abilities: Array<Object>
+    moves: Array<Object>
+    sprites: Array<Object>
+    species: Array<Object>
+    stats: Array<Object>
+    types: Array<Object>
+}
