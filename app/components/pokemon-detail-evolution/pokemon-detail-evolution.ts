716b144a2e3bdc150f3c53f811ae70dd8866d69d
loiane <loianeg@gmail.com>
2016-09-14 10:13:52 -0300

    refactor due pokemon navigation arrows

--- app/components/pokemon-detail-evolution/pokemon-detail-evolution.ts
+++ app/components/pokemon-detail-evolution/pokemon-detail-evolution.ts
@@ -1,43 +1,51 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 import { PokedexService } from '../../providers/pokedex-service/pokedex-service';
+import { Utilities } from '../../util/utilities';
 import { Capitalize } from '../../pipes/capitalize';
 import { PokeNumber } from '../../pipes/pokeNumber';
 
 @Component({
   selector: 'pokemon-detail-evolution',
   templateUrl: 'build/components/pokemon-detail-evolution/pokemon-detail-evolution.html',
-  providers: [ PokedexService ],
   pipes: [ Capitalize, PokeNumber ]
 })
-export class PokemonDetailEvolution implements OnInit {
+export class PokemonDetailEvolution implements OnChanges {
 
     @Input() pokemon: any;
     evolutionDetails: any;
     evolutions: any[] = [];
 
-    constructor(private pokedexService: PokedexService) {}
+    private imgBaseUrl: string = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other-sprites/official-artwork/';
 
-    ngOnInit(){
-      this.pokedexService.getEvolutions()
-        .subscribe(evolutionDetails => this.evolutionDetails = evolutionDetails);
+    constructor(
+      private pokedexService: PokedexService,
+      private util: Utilities
+    ) {}
+
+    ngOnChanges(){
+      let speciesId: number = this.util.retrieveIdFromUrl(this.pokemon.species.url, 'pokemon-species');
+      this.pokedexService.getSpecies(speciesId)
+        .subscribe(speciesDetails => this.loadEvolutionDetails(speciesDetails));
     }
 
-    getEvolutions(){
-      if (this.evolutionDetails && this.evolutionDetails.chain
-        && this.evolutions.length == 0){
-        let item = this.evolutionDetails.chain.evolves_to[0];
-        while (item){
-          let ev: any = {};
-          ev.name = item.species.name;
-          ev.id = item.species.url.replace("http://pokeapi.co/api/v2/pokemon-species/", "");
-          ev.id = ev.id.replace("/","");
-          ev.level = item.evolution_details[0].min_level;
-          this.evolutions.push(ev);
-          item = item.evolves_to[0];
-        }
+    loadEvolutionDetails(speciesDetails){
+      let id: number = this.util.retrieveIdFromUrl(speciesDetails.evolution_chain.url, 'evolution-chain');
+      this.pokedexService.getEvolution(id)
+        .subscribe(data => this.loadEvolutions(data));
+    }
 
+    loadEvolutions(data){
+      this.evolutionDetails = data;
+      this.evolutions = [];
+      let item = this.evolutionDetails.chain.evolves_to[0];
+      while (item){
+        let ev: any = {};
+        ev.name = item.species.name;
+        ev.id = this.util.retrieveIdFromUrl(item.species.url, 'pokemon-species');
+        ev.level = item.evolution_details[0].min_level;
+        this.evolutions.push(ev);
+        item = item.evolves_to[0];
       }
-      return this.evolutions;
     }
 }
