2fa155d05e6e470ba9a15595096b3dddbd10bdbb
loiane <loianeg@gmail.com>
2016-09-14 10:12:26 -0300

    refactor due pokemon navigation arrows

--- app/components/pokemon-detail-species/pokemon-detail-species.ts
+++ app/components/pokemon-detail-species/pokemon-detail-species.ts
@@ -1,67 +1,42 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 import { PokedexService } from '../../providers/pokedex-service/pokedex-service';
 import { Capitalize } from '../../pipes/capitalize';
+import { SplitCapitalize } from '../../pipes/split-capitalize';
+import { DivBy10 } from '../../pipes/div-by-10';
+import { PokemonGender } from '../../pipes/pokemon-gender';
+import { Utilities } from '../../util/utilities';
 import { PokeNumber } from '../../pipes/pokeNumber';
 import { PokemonDetailTypeEffectiveness } from '../pokemon-detail-type-effectiveness/pokemon-detail-type-effectiveness';
 
 @Component({
   selector: 'pokemon-detail-species',
   templateUrl: 'build/components/pokemon-detail-species/pokemon-detail-species.html',
-  providers: [ PokedexService ],
-  pipes: [ Capitalize, PokeNumber ],
+  pipes: [ Capitalize, PokeNumber, SplitCapitalize, DivBy10, PokemonGender ],
   directives: [ PokemonDetailTypeEffectiveness ]
 })
-export class PokemonDetailSpecies implements OnInit {
+export class PokemonDetailSpecies implements OnChanges {
 
   @Input() pokemon: any;
-  @Input() types: any;
-  speciesDetails: any;
+  private speciesDetails: any;
+  private tab: string = 'pokedex';
+  speciesName: string;
+  description: string;
 
-  tab: string = 'pokedex';
+  constructor(
+    private pokedexService: PokedexService,
+    private util: Utilities
+  ) {}
 
-  constructor(private pokedexService: PokedexService) {}
-
-  ngOnInit(){
-    this.pokedexService.getSpecies(null)
-      .subscribe(speciesDetails => this.speciesDetails = speciesDetails);
+  ngOnChanges(){
+    let speciesId: number = this.util.retrieveIdFromUrl(this.pokemon.species.url, 'pokemon-species');
+    this.pokedexService.getSpecies(speciesId)
+      .subscribe(data => this.initSpeciesData(data));
   }
 
-  getHeight(){
-    return this.pokemon.height / 10;
-  }
-
-  getWeight(){
-    return this.pokemon.weight / 10;
-  }
-
-  getGender(){
-    let femalePerc = this.speciesDetails.gender_rate / 0.08;
-    let malePerc = 100 - femalePerc;
-    return malePerc + '% male, ' + femalePerc + '% female';
-  }
-
-  getSpeciesName(){
-    if (this.speciesDetails){
-      let listNames = this.speciesDetails.genera;
-      for (let i=0; i<listNames.length; i++){
-        if (listNames[i].language.name === 'en'){
-          return listNames[i].genus;
-        }
-      }
-    }
-    return undefined;
-  }
-
-  getDescription(){
-    if (this.speciesDetails){
-      let list = this.speciesDetails.flavor_text_entries;
-      for (let i=0; i<list.length; i++){
-        if (list[i].language.name === 'en'){
-          return list[i].flavor_text;
-        }
-      }
-    }
-    return undefined;
+  initSpeciesData(data){
+    this.speciesDetails = data;
+    this.speciesName = this.util.getTranslatedName(this.speciesDetails.genera);
+    this.description = this.util.getTranslatedName(this.speciesDetails.flavor_text_entries);
   }
 }
