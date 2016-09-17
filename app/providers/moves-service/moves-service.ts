5e406475a134bb52423991b2bfd020c29eeec5c7
loiane <loianeg@gmail.com>
2016-09-17 00:09:31 -0300

    removed unused method

--- app/providers/moves-service/moves-service.ts
+++ app/providers/moves-service/moves-service.ts
@@ -7,9 +7,4 @@ export class MovesService {
 
   constructor(private http: Http) {}
 
-  getMoves(){
-    return this.http.get('data/v2/moves.json')
-    //this.http.get('http://pokeapi.co/api/v2/pokemon/?limit=20')
-      .map((res: Response) => res.json().results);
-  }
 }
