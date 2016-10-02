28a4e91b9dbdb4f6811aaf1449fe6e051392c40b
loiane <loianeg@gmail.com>
2016-10-02 12:43:05 -0300

    updated to ionic 2.0.0-rc.0 - pokemon type list

--- src/pages/type-list/type-list.ts
+++ src/pages/type-list/type-list.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { PokedexService } from '../../providers/pokedex-service';
-import { Capitalize } from '../../pipes/capitalize';
-
-import { Loader } from '../../components/loader/loader';
 
 @Component({
   templateUrl: 'type-list.html'
