5f8e1d813cc7ed80b3610c08a2962e68d31c7d92
loiane <loianeg@gmail.com>
2016-10-02 12:50:35 -0300

    removed unused import

--- src/components/pokemon-details/pokemon-detail-move-detail/pokemon-detail-move-detail.ts
+++ src/components/pokemon-details/pokemon-detail-move-detail/pokemon-detail-move-detail.ts
@@ -1,7 +1,5 @@
 import { Component, Input } from '@angular/core';
 
-import { PokemonDetailMoveDetailRow } from '../pokemon-detail-move-detail-row/pokemon-detail-move-detail-row';
-
 @Component({
   selector: 'pokemon-detail-move-detail',
   templateUrl: 'pokemon-detail-move-detail.html'
