bd89d87b628f7657959f13f44da8258d8f6136f5
loiane <loianeg@gmail.com>
2016-10-02 14:11:37 -0300

    pokemon-list: added infinite scroll

--- src/pages/pokemon-list/pokemon-list.ts
+++ src/pages/pokemon-list/pokemon-list.ts
@@ -11,7 +11,10 @@ import { PokemonDetail } from '../pokemon-detail/pokemon-detail';
 export class PokemonList implements OnInit {
 
   private pokemonList: any[] = [];
+  private pokemons: any[] = [];
   private searchQuery: string = '';
+  private start: number = 0;
+  private limit: number = 15;
 
   constructor(
     private navCtrl: NavController,
@@ -20,16 +23,27 @@ export class PokemonList implements OnInit {
 
   ngOnInit(){
     this.pokedexService.getAllPokemon()
-      .subscribe(pokemonList => {
-        this.pokemonList = pokemonList;
+      .subscribe(data => {
+        this.pokemonList = data;
+        this.loadData();
         //this.pokedexService.initData();
-      });
+    });
+  }
+
+  loadData(){
+    return new Promise(resolve => {
+      let list = this.pokemonList.splice(this.start, this.start + this.limit);
+      for (let item of list){
+        this.pokemons.push(item);
+      }
+      resolve(true);
+    });
   }
 
   getPokemons(){
     var q = this.searchQuery;
     if (q.trim() == '') {
-        return this.pokemonList;
+        return this.pokemons;
     }
     return this.pokemonList.filter((v) => {
         if (v.name.toLowerCase().indexOf(q.toLowerCase()) >= 0) {
@@ -37,6 +51,24 @@ export class PokemonList implements OnInit {
         }
         return false;
     });
+  }
+
+  doInfinite(infiniteScroll:any) {
+     console.log('doInfinite, start is currently '+this.start);
+
+     if (this.start + this.limit < 151){
+       this.start += this.limit;
+
+       if (this.start == 150){
+         this.start = 151;
+       }
+
+       this.loadData().then(()=>{
+         infiniteScroll.complete();
+       });
+     } else {
+       infiniteScroll.complete();
+     }
   }
 
   goToPokemonDetail(event){
