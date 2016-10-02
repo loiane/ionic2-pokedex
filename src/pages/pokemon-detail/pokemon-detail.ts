21f9eea172c0c0f651968386dd6bb9643942324f
loiane <loianeg@gmail.com>
2016-10-02 12:42:48 -0300

    updated to ionic 2.0.0-rc.0 - pokemon detail

--- src/pages/pokemon-detail/pokemon-detail.ts
+++ src/pages/pokemon-detail/pokemon-detail.ts
@@ -2,15 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { PokedexService } from '../../providers/pokedex-service';
-import { Capitalize } from '../../pipes/capitalize';
-import { PokeNumber } from '../../pipes/pokeNumber';
-import { PokemonImage } from '../../pipes/pokemon-image';
-//import { PokemonDetailSpecies } from '../../components/pokemon-detail-species/pokemon-detail-species';
-//import { PokemonDetailSprites } from '../../components/pokemon-detail-sprites/pokemon-detail-sprites';
-//import { PokemonDetailAbilities } from '../../components/pokemon-detail-abilities/pokemon-detail-abilities';
-//import { PokemonDetailStats } from '../../components/pokemon-detail-stats/pokemon-detail-stats';
-//import { PokemonDetailEvolution } from '../../components/pokemon-detail-evolution/pokemon-detail-evolution';
-//import { PokemonDetailMoves } from '../../components/pokemon-detail-moves/pokemon-detail-moves';
 
 @Component({
   templateUrl: 'pokemon-detail.html'
