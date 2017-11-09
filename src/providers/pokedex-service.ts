2fc9cec162902ed271c6485103eb434a184b01c0
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2017-11-09 16:36:46 -0200

    [UPDATE-IONIC] Update ionic and project libs, like angular

--- src/providers/pokedex-service.ts
+++ src/providers/pokedex-service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
-import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/forkJoin'
 
 @Injectable()
 export class PokedexService {
